f808cbeafd8a2dcdb366d282bd41f1a6c50b653a
Jacob Stephens <jacob.stephens@gordon.edu>
2020-11-23 11:51:17 -0500

    revised about page bar

--- front-page.php
+++ front-page.php
@@ -10,19 +10,25 @@ namespace WP_Rig\WP_Rig;
 get_header();
 ?>
 			<div class="bar">
-				<div class="image-frame">
+				<div class="frame">
 					<div class="image-position">
-						<img src="/wp-content/themes/wprig-2.0.1/assets/images/AI_Magazine_Spr17_970x211.jpg" width="970" height="211" />
-						<div class="clients">
-							2490 Hopewell Rd&ensp;|&ensp;
-							Elverson, PA 19520&ensp;|&ensp;
-							610.780.2023&ensp;|&ensp;
-							© 2020 Melodee Stephens
-						</div>
+						<img src="//goodness-creative.local/wp-content/uploads/2020/11/cover-angled-archmerean20-strip.png" width="970" height="211" />
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+
+<main id="primary" class="site-main">
+	<div class="container">
+		<?php
+		while ( have_posts() ) {
+			the_post();
+			get_template_part( 'template-parts/content/entry', get_post_type() );
+		}
+		?>
+	</div>
+</main>
+
 </body>
 </html>
--- page-about.php
+++ page-about.php
@@ -13,13 +13,12 @@ get_header();
 <div class="bar">
 	<div class="image-frame" style="padding-left: 0px;">
 		<div class="about-image-position">
-			<img src="http://goodness-creative.local/wp-content/uploads/2020/11/Melodee-1-5-ratio.jpg" height="211" />
-			<img style="padding-left: 10px" src="http://goodness-creative.local/wp-content/uploads/2020/11/Auction18_Invitation_652x211.jpg" height="211" />
-			<img style="padding-left: 7px; padding-right: 2px; float: right;" src="http://goodness-creative.local/wp-content/uploads/2020/11/Jacob-smiling-1-5-zoom.jpg" height="211" />
+			<img style="padding-right: 10px;" src="http://goodness-creative.local/wp-content/uploads/2020/11/Auction18_Invitation_652x211.jpg" height="211" />
+			<img src="http://goodness-creative.local/wp-content/uploads/2020/11/Melodee-1-5-ratio-bw.jpg" height="211" />
+			<img style="padding-left; float: right;" src="http://goodness-creative.local/wp-content/uploads/2020/11/Jacob-smiling-1-5-bw.jpg" height="211" />
 		</div>
 	</div>
 </div>
-
 
 <main id="primary" class="site-main">
 	<div class="container">
